b668de0a5fcd5ba2ea366e977b36bf9f96078ffa
vrajendren <vrajendren86@gmail.com>
2024-10-17 13:02:07 -0400

    Add files via upload
    Day  1 Assignment

---
+++ HelloTestLeaf.java
@@ -0,0 +1,13 @@
+package week.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Welcome to Test Leaf");
+ System.out.println("Day 1 Assignment 1");
+ System.out.println("Rajendren");
+ 
+	}
+
+}
