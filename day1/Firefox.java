061c82fa98d2319f6c210f5ca3326f578ce57594
vrajendren <vrajendren86@gmail.com>
2024-10-17 13:04:02 -0400

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package week.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//To store value to the variable
+// datatype variableName = value;
+		
+float browserVersion = 100.2f ;
+String browserName = "Firefox";
+boolean isVisible = true;
+int releaseYear=1998; 
+char browserLogo = 'f';
+System.out.println("browserVersion is: = "+browserVersion);
+System.out.println("Release Year is :"+releaseYear);
+System.out.println("Browser Name is : "+browserName);
+System.out.println("Is visible :"+isVisible);
+System.out.println("Browser Logo - "+browserLogo);
+
+	}
+}
